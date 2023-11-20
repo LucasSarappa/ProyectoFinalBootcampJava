4d0657fef9c14047805395bea5fe1ff253d6b4b1
Lucas Sarappa <lsarappa@frba.utn.edu.ar>
2023-11-20 18:36:13 -0300

    Delete api-rest directory

--- api-rest/src/main/java/com/bootcamp/apirest/exception/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.bootcamp.apirest.exception;
-import org.springframework.http.HttpStatus;
-
-public class UserNotFoundException extends ApiException {
-
-    public UserNotFoundException(Long dni) {
-        super(HttpStatus.NOT_FOUND, "User not found with DNI: " + dni);
-    }
-}
