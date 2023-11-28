d756e3bc1da547101bf68674f56ea8a8f94f437c
Lucas Sarappa <lsarappa@frba.utn.edu.ar>
2023-11-28 12:27:02 -0300

    Update AddUser.js

--- front_api_crud/src/users/AddUser.js
+++ front_api_crud/src/users/AddUser.js
@@ -26,47 +26,40 @@ export default function AddUser() {
   };
 
   const hasSpacesFollowedByLetter = (input) => {
-    // Verificar si hay espacios seguidos por al menos una letra
+    // Verifico si hay espacios seguidos por al menos una letra
     return /^(?!\s)(?!.*\s{2,})(?!.*\s$)[a-zA-Z\s]+$/.test(input);
   };
 
   const onSubmit = async (e) => {
     e.preventDefault();
 
-    // Validar el formulario antes de enviar la solicitud
     if (!isFormValid()) {
       setErrorMessage("Error, please fill in all required fields");
       return;
     }
 
-    // Validar que no haya espacios al principio o al final de nombre y apellido
     if (first_name.trim() !== first_name || last_name.trim() !== last_name) {
       setErrorMessage("Error, name and last name should not have leading or trailing spaces");
       return;
     }
 
-    // Validar que no haya espacios en blanco en nombre, apellido y trabajo, a menos que estén seguidos por una letra
     if (!hasSpacesFollowedByLetter(first_name) || !hasSpacesFollowedByLetter(last_name) || !hasSpacesFollowedByLetter(job)) {
       setErrorMessage("Error, name, last name, and job should not contain spaces, except when followed by a letter");
       return;
     }
 
-    // Validar que el DNI tenga entre 7 y 8 números
     if (dni.length < 7 || dni.length > 8) {
       setErrorMessage("Error, the DNI should have between 7 and 8 numbers");
       return;
     }
 
     try {
-      // Realizar la solicitud a la API de validación
       const validationResponse = await axios.get(`http://localhost:8081/validate/dni/${dni}`);
 
-      // Si la validación es exitosa, entonces realizar la solicitud a la API del crud
       if (validationResponse.status === 200) {
         const saveUserResponse = await axios.post("http://localhost:8080/users", user);
         console.log("Save User Response:", saveUserResponse.data);
 
-        // Si todo sale bien, navegar a la página principal
         navigate("/");
       } else {
         // Manejar la respuesta de validación incorrecta
@@ -78,13 +71,10 @@ export default function AddUser() {
       }
     } catch (error) {
       if (error.response) {
-        // Manejar errores de respuesta aquí
         setErrorMessage(error.response.data.message);
       } else if (error.request) {
-        // Manejar errores de solicitud aquí
         setErrorMessage("Error making the request");
       } else {
-        // Manejar otros tipos de errores aquí
         setErrorMessage("An unexpected error occurred");
       }
     }
