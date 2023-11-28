923c53818edbd55859276c068f9ae20891a13ce6
Lucas Sarappa <lsarappa@frba.utn.edu.ar>
2023-11-28 10:22:26 -0300

    Update UserController.java

--- api-rest-crud/src/main/java/com/bootcamp/apirest/controller/UserController.java
+++ api-rest-crud/src/main/java/com/bootcamp/apirest/controller/UserController.java
@@ -5,59 +5,54 @@ import com.bootcamp.apirest.model.User;
 import com.bootcamp.apirest.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@CrossOrigin({"http://localhost:3000/", "http://localhost:8081"})
-
+@CrossOrigin({"http://localhost:3000", "http://localhost:8081"})
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @Autowired
-    private RestTemplate restTemplate; // Necesitarás un bean RestTemplate para realizar solicitudes a la API de validación
+    private RestTemplate restTemplate; // RestTemplate para realizar solicitudes a la API de validación
 
     @PostMapping("/users")
     public ResponseEntity<Object> newUser(@Valid @RequestBody User newUser) {
         try {
-            // Antes de guardar el usuario, realiza la validación utilizando la API de validación
-            validateDni(newUser.getDni());
+            // Antes de guardar el usuario, realizo la validación usando la API de validación mandando el dni del nuevo usuario
+            ResponseEntity<String> validationResponse = restTemplate.getForEntity("http://localhost:8081/validate/dni/" + newUser.getDni(), String.class);
 
-            User savedUser = userService.saveUser(newUser);
-            return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+            if (validationResponse.getStatusCode() == HttpStatus.OK) {
+                User savedUser = userService.saveUser(newUser);
+                return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+            } else {
+                // Manejar la respuesta de validación incorrecta
+                return new ResponseEntity<>("Invalid DNI", HttpStatus.BAD_REQUEST);
+            }
         } catch (DuplicateDniException e) {
             return new ResponseEntity<>(new ApiError(HttpStatus.BAD_REQUEST, e.getMessage()), HttpStatus.BAD_REQUEST);
         }
     }
 
-    private void validateDni(Long dni) {
-        // Realiza una solicitud a la API de validación para validar el DNI
-        ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://localhost:8081/validate/dni/" + dni, String.class);
-
-        if (responseEntity.getStatusCode() != HttpStatus.OK) {
-            throw new RuntimeException("Invalid DNI");
-        }
-    }
-
-
     @GetMapping("/users")
-    List<User> getAllUsers() {
+    public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
     @GetMapping("/users/{dni}")
-    Optional<User> getUserByDni(@PathVariable Long dni) {
+    public Optional<User> getUserByDni(@PathVariable Long dni) {
         return userService.getUserByDni(dni);
     }
 
     @PutMapping("/users/{dni}")
-    User updateUser(@RequestBody User newUser, @PathVariable Long dni) {
+    public User updateUser(@RequestBody User newUser, @PathVariable Long dni) {
         return userService.updateUser(dni, newUser);
     }
 
