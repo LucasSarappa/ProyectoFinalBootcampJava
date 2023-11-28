e69e725007bcb44837684118df2f78e7915792f5
Lucas Sarappa <lsarappa@frba.utn.edu.ar>
2023-11-28 10:23:45 -0300

    Update AddUser.js

--- front_api_crud/src/users/AddUser.js
+++ front_api_crud/src/users/AddUser.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
+
 export default function AddUser() {
   let navigate = useNavigate();
 
@@ -21,24 +22,73 @@ export default function AddUser() {
   };
 
   const isFormValid = () => {
-    return dni !== '' && first_name !== '' && last_name !== '' && job !== '';
+    return dni !== '' && first_name.trim() !== '' && last_name.trim() !== '' && job.trim() !== '';
+  };
+
+  const hasSpacesFollowedByLetter = (input) => {
+    // Verificar si hay espacios seguidos por al menos una letra
+    return /^(?!\s)(?!.*\s{2,})(?!.*\s$)[a-zA-Z\s]+$/.test(input);
   };
 
   const onSubmit = async (e) => {
     e.preventDefault();
 
+    // Validar el formulario antes de enviar la solicitud
+    if (!isFormValid()) {
+      setErrorMessage("Error, please fill in all required fields");
+      return;
+    }
+
+    // Validar que no haya espacios al principio o al final de nombre y apellido
+    if (first_name.trim() !== first_name || last_name.trim() !== last_name) {
+      setErrorMessage("Error, name and last name should not have leading or trailing spaces");
+      return;
+    }
+
+    // Validar que no haya espacios en blanco en nombre, apellido y trabajo, a menos que estén seguidos por una letra
+    if (!hasSpacesFollowedByLetter(first_name) || !hasSpacesFollowedByLetter(last_name) || !hasSpacesFollowedByLetter(job)) {
+      setErrorMessage("Error, name, last name, and job should not contain spaces, except when followed by a letter");
+      return;
+    }
+
+    // Validar que el DNI tenga entre 7 y 8 números
+    if (dni.length < 7 || dni.length > 8) {
+      setErrorMessage("Error, the DNI should have between 7 and 8 numbers");
+      return;
+    }
+
     try {
-      await axios.post("http://localhost:8080/users", user);
-      navigate("/");
+      // Realizar la solicitud a la API de validación
+      const validationResponse = await axios.get(`http://localhost:8081/validate/dni/${dni}`);
+
+      // Si la validación es exitosa, entonces realizar la solicitud a la API del crud
+      if (validationResponse.status === 200) {
+        const saveUserResponse = await axios.post("http://localhost:8080/users", user);
+        console.log("Save User Response:", saveUserResponse.data);
+
+        // Si todo sale bien, navegar a la página principal
+        navigate("/");
+      } else {
+        // Manejar la respuesta de validación incorrecta
+        if (validationResponse.data.status === "error") {
+          setErrorMessage(validationResponse.data.message);
+        } else {
+          setErrorMessage("Error during validation");
+        }
+      }
     } catch (error) {
       if (error.response) {
-        if (error.response.status === 400) {
-          setErrorMessage("Error, check the included data and file it again")
+        // Manejar errores de respuesta aquí
+        setErrorMessage(error.response.data.message);
+      } else if (error.request) {
+        // Manejar errores de solicitud aquí
+        setErrorMessage("Error making the request");
+      } else {
+        // Manejar otros tipos de errores aquí
+        setErrorMessage("An unexpected error occurred");
       }
     }
-  } 
   };
-
 
   return (
     <div className="container">
@@ -116,4 +166,3 @@ export default function AddUser() {
     </div>
   );
 }
-
